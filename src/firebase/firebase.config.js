64ab24f9b62421e733edfc69b589d143944f6ee6
aline-necchi <alinenecchi@gmail.com>
2022-11-23 23:29:26 -0300

    adiciona formulario addTasks

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -20,4 +21,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-export { app, auth };
+const db = getFirestore(app);
+
+export { app, auth, db };
